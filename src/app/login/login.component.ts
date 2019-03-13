721e8ee657a75077fa1eb665d7e75fe8e96e9578
ywager <441200288@qq.com>
2019-03-13 20:02:40 +0800

    Add login page form validation
    Angular Form Validation:
    https://angular.io/guide/form-validation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,15 +8,21 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
+  form: FormGroup;
   constructor(
+    private fb: FormBuilder,
     private router: Router
   ) { }
 
   ngOnInit() {
+    this.form = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
   }
 
   submit(): void {
+    console.log(this.form.value);
     this.router.navigate(['/']);
   }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { FlexLayoutModule } from '@angular/flex-layout';
+
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -10,6 +13,8 @@ import { MatButtonModule } from '@angular/material/button';
   declarations: [],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     FlexLayoutModule,
     MatCardModule,
     MatFormFieldModule,
@@ -17,6 +22,8 @@ import { MatButtonModule } from '@angular/material/button';
     MatButtonModule
   ],
   exports: [
+    FormsModule,
+    ReactiveFormsModule,
     FlexLayoutModule,
     MatCardModule,
     MatFormFieldModule,
