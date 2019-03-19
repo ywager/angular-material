492621887c41fbc5c6a2c76206a6dd7afa60749b
ywager <441200288@qq.com>
2019-03-19 19:50:03 +0800

    Fix search component styles
    Use the angular material icons instead of 'Font-Awesome'
    Adjust layout with angular 'flex-layout'

--- src/app/shared/components/mat-magic-search/mat-magic-search.module.ts
+++ src/app/shared/components/mat-magic-search/mat-magic-search.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 import { MatMagicSearchComponent } from './mat-magic-search.component';
 import { MatMagicSearchDirective } from './directive/mat-magic-search.directive';
 import { RendererService } from './services/renderer.service';
@@ -10,7 +14,10 @@ import { RendererService } from './services/renderer.service';
   imports: [
     CommonModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FlexLayoutModule,
+    MatIconModule,
+    MatInputModule
   ],
   providers: [RendererService],
   exports: [MatMagicSearchComponent]
